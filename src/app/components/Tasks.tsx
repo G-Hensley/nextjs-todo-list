53ecc66e7e84f23785f8f41a1b77cbd33587cee5
Gavin Hensley <gavinhensley@protonmail.com>
2025-03-04 05:11:23 -0500

    Added local storage and delete functionality

--- src/app/components/Tasks.tsx
+++ src/app/components/Tasks.tsx
@@ -1,13 +1,18 @@
 import { Task } from "../types";
 
-export default function Tasks( { tasks }: { tasks: Task[] } ) {
+export default function Tasks( { tasks, setTasks }: { tasks: Task[], setTasks: (tasks: Task[]) => void } ) {
     return (
         <div className="flex flex-col gap-2 items-center">
             <h2 className="text-2xl font-bold">Tasks</h2>
             {tasks.map((task) => {
                 return (
-                    <div className="bg-slate-800 px-4 py-2 rounded-md text-center w-auto" key={task.id}>
+                    <div className="bg-slate-800 px-4 py-2 rounded-md text-center w-auto flex flex-col gap-4" key={task.id}>
                         <h2>{task.text}</h2>
+                        <button 
+                            className="bg-red-900 hover:bg-red-800 transition-colors p-2 rounded-md 
+                            text-center w-fit cursor-pointer self-center" onClick={() => {
+                            setTasks(tasks.filter((t) => t.id !== task.id));
+                        }}>Delete</button>
                     </div>
                 )
             })}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
 'use client';
 
-import Tasks from "@/app/components/Tasks";
-import CreateTask from "@/app/components/CreateTask";
-import { Task } from "./types";
-import { useState } from "react";
+import Tasks from '@/app/components/Tasks';
+import CreateTask from '@/app/components/CreateTask';
+import { Task } from './types';
+import { useState, useEffect } from 'react';
+
+const getTasks = () => {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    return JSON.parse(storedTasks);
+  }
+  return [];
+};
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTasks(getTasks());
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <main className='flex flex-col gap-8 p-4 items-center h-screen'>
       <header>
         <h1 className='text-4xl font-bold'>Todo List</h1>
       </header>
-      <CreateTask tasks={tasks} setTasks={setTasks}/>
-      <Tasks tasks={tasks}/>
+      <CreateTask tasks={tasks} setTasks={setTasks} />
+      <Tasks tasks={tasks} setTasks={setTasks}/>
     </main>
   );
 }
